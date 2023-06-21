64cf459a9a9cfa1616905916d94d484c0be89d0b
Peng Ai <aaaipeng@gmail.com>
2023-06-21 00:15:40 -0700

    Add styles to the frontend ui

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,46 +2,77 @@ import { useState, useContext } from 'react'
 import axios from 'axios'
 import { Context } from '../App'
 import { useNavigate } from 'react-router-dom'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
   const { setUserInfo } = useContext(Context)
   const navigate = useNavigate()
 
   const loginHandler = async (e) => {
     e.preventDefault()
     if (!email || !password) {
-      return
+      return setMessage('Please enter all the fields above.')
     }
+    setMessage('')
     try {
       const { data: user } = await axios.post('/api/users/auth', { email, password })
       setUserInfo(user)
       localStorage.setItem('userInfo', JSON.stringify(user))
       navigate('/', { replace: true })
     } catch (error) {
+      setMessage(error.response.data.message)
       console.log('error:', error.response.data.message)
     }
-
   }
+
+  const onChange = (e) => {
+    setMessage('')
+    switch (e.target.name) {
+      case 'email':
+        setEmail(e.target.value)
+        break;
+      case 'password':
+        setPassword(e.target.value)
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <main>
-      <div className='form-card'>
-        <h2>Login</h2>
+    <Container>
+      <div className='form'>
         <form onSubmit={loginHandler}>
-          <div className='form-input'>
-            <label htmlFor="email">Email</label>
-            <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className='form-input'>
-            <label htmlFor="password">Password</label>
-            <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <div className='form-action'>
-            <button type='submit'>Submit</button>
-          </div>
+          <Typography variant='h6' gutterBottom>
+            Login
+          </Typography>
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type='email' id="email" label="Email" value={email} name='email' variant="outlined" onChange={onChange} />
+          </FormControl >
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type='password' id="password" label="Password" value={password} name='password' variant="outlined" onChange={onChange} />
+          </FormControl>
+          <FormControl margin='normal' fullWidth>
+            <Button type='submit' variant='contained'>Submit</Button>
+          </FormControl>
+          {message ?
+            <p className='form-msg'>
+              {`* ${message}`}
+            </p>
+            : null
+          }
+          <p className='redirect-msg'>
+            Don't have an account? <a href="/register">Register</a> now.
+          </p>
         </form>
       </div>
-    </main>
+    </Container>
   )
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,19 +2,28 @@ import { useState, useContext } from 'react'
 import axios from 'axios'
 import { Context } from '../App'
 import { useNavigate } from 'react-router-dom'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 
 export default function Register() {
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [message, setMessage] = useState('')
   const { setUserInfo } = useContext(Context)
   const navigate = useNavigate()
 
   const registerHandler = async (e) => {
     e.preventDefault()
-    if (!name || !email || !password || !passwordConfirm || password !== passwordConfirm) {
-      return
+    if (!name || !email || !password || !passwordConfirm) {
+      return setMessage('Please enter all the fields above.')
+    }
+    if (password !== passwordConfirm) {
+      return setMessage('Passwords don\'t match.')
     }
     try {
       const newUser = { name, email, password }
@@ -23,35 +32,64 @@ export default function Register() {
       localStorage.setItem('userInfo', JSON.stringify(user))
       navigate('/', { replace: true })
     } catch (error) {
+      setMessage(error.response.data.message)
       console.log('error:', error.response.data.message)
     }
   }
+
+  const onChange = (e) => {
+    setMessage('')
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value)
+        break;
+      case 'email':
+        setEmail(e.target.value)
+        break;
+      case 'password':
+        setPassword(e.target.value)
+        break;
+      case 'passwordConfirm':
+        setPasswordConfirm(e.target.value)
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <main>
-      <div className='form-card'>
-        <h2>Register</h2>
+    <Container>
+      <div className='form'>
         <form onSubmit={registerHandler}>
-          <div className='form-input'>
-            <label htmlFor="name">Name</label>
-            <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)} />
-          </div>
-          <div className='form-input'>
-            <label htmlFor="email">Email</label>
-            <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className='form-input'>
-            <label htmlFor="password">Password</label>
-            <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <div className='form-input'>
-            <label htmlFor="passwordConfirm">Confirm Password</label>
-            <input type="password" name='passwordConfirm' value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
-          </div>
-          <div className='form-action'>
-            <button type='submit'>Submit</button>
-          </div>
+          <Typography variant='h6' gutterBottom>
+            Register
+          </Typography>
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type='text' id="name" label="Name" value={name} name='name' variant="outlined" onChange={onChange} />
+          </FormControl >
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type='email' id="email" label="Email" value={email} name='email' variant="outlined" onChange={onChange} />
+          </FormControl >
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type="password" id="password" label="Password" value={password} name='password' variant="outlined" onChange={onChange} />
+          </FormControl >
+          <FormControl margin='normal' fullWidth>
+            <TextField size='small' type="password" id="passwordConfirm" label="Confirm Password" value={passwordConfirm} name='passwordConfirm' variant="outlined" onChange={onChange} />
+          </FormControl >
+          <FormControl margin='normal' fullWidth>
+            <Button type='submit' variant='contained'>Submit</Button>
+          </FormControl>
+          {message ?
+            <p className='form-msg'>
+              {`* ${message}`}
+            </p>
+            : null
+          }
+          <p className='redirect-msg'>
+            Already have an account? <a href="/login">Login</a> now.
+          </p>
         </form>
       </div>
-    </main>
+    </Container>
   )
 }
